57aec9a19a90ea41104b80501017867a5c4c3c18
c00kamunga <tsothalanti2@yahoo.com>
2020-05-01 17:43:42 -0700

    made an API call to smurfs endpoint dispatching type GETSMURFS_SUCCESS  and type GETSMURFS_FAILED

--- smurfs/src/actions/actions.js
+++ smurfs/src/actions/actions.js
@@ -1,11 +1,23 @@
-import axios from 'axios';
+import axios from "axios";
 
-export const GETSMURFS_START = 'GETSMURFS_START'
-export const GETSMURFS_SUCCESS = 'GETSMURFS_SUCCESS'
-export const GETSMURFS_FAILED = 'GETSURFS_FAILED'
+export const GETSMURFS_START = "GETSMURFS_START";
+export const GETSMURFS_SUCCESS = "GETSMURFS_SUCCESS";
+export const GETSMURFS_FAILED = "GETSURFS_FAILED";
 
-export const ADDSMURF_START = 'ADDSMURF_START'
-export const ADDSMURF_SUCCESS = 'ADDSMURF_SUCCESS'
-export const ADDSMURF_FAILED = 'ADDSMURF_FAILED'
+export const ADDSMURF_START = "ADDSMURF_START";
+export const ADDSMURF_SUCCESS = "ADDSMURF_SUCCESS";
+export const ADDSMURF_FAILED = "ADDSMURF_FAILED";
 
-
+export function getSmurfs() {
+  return (dispatch) => {
+    axios
+      .get("http://localhost:3333/smurfs")
+      .then((res) => {
+        dispatch({ type: GETSMURFS_SUCCESS, payload: res.data });
+      })
+      .catch((err) => {
+        console.log(err);
+        dispatch({ type: GETSMURFS_FAILED, payload: err.response.data });
+      });
+  };
+}
