1cfddd319c34d597bf5f5d0ecba9b27776642e39
c00kamunga <tsothalanti2@yahoo.com>
2020-05-01 17:19:29 -0700

    wrapped App with provider and passed in store as a prop to provider

--- smurfs/src/index.js
+++ smurfs/src/index.js
@@ -2,17 +2,17 @@ import React from "react";
 import ReactDOM from "react-dom";
 import "./index.css";
 import App from "./components/App";
+import thunk from 'redux-thunk';
 import { createStore, applyMiddleware } from 'redux';
 import { Provider } from 'react-redux';
-import thunk from 'redux-thunk'
 
 
 const store = createStore(
 reducer,
 applyMiddleware(thunk)
-)
+);
 
 
 
 
-ReactDOM.render(<App />, document.getElementById("root"));
+ReactDOM.render(<Provider store={store}><App /></Provider>, document.getElementById("root"));
