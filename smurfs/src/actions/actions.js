7d9cb85dbee8a670f1fc39ce3738c3b25fc8473b
c00kamunga <tsothalanti2@yahoo.com>
2020-05-01 17:33:26 -0700

    created actions for retrieving smurfs and posting smurfs

---
+++ smurfs/src/actions/actions.js
@@ -0,0 +1,11 @@
+import axios from 'axios';
+
+export const GETSMURFS_START = 'GETSMURFS_START'
+export const GETSMURFS_SUCCESS = 'GETSMURFS_SUCCESS'
+export const GETSMURFS_FAILED = 'GETSURFS_FAILED'
+
+export const ADDSMURF_START = 'ADDSMURF_START'
+export const ADDSMURF_SUCCESS = 'ADDSMURF_SUCCESS'
+export const ADDSMURF_FAILED = 'ADDSMURF_FAILED'
+
+
