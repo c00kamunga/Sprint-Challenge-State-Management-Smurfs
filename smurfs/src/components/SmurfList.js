05ef1598d3cacba475c769e9a08bbe0bbe2ef047
c00kamunga <tsothalanti2@yahoo.com>
2020-05-01 18:52:46 -0700

    finished SmurfList component where the smurfs information is listed

---
+++ smurfs/src/components/SmurfList.js
@@ -0,0 +1,37 @@
+import React from 'react';
+import { connect } from 'react-redux'
+
+class SmurfList extends React.Component {
+    componentDidMount(){
+        this.props.getSmurfs();
+    }
+
+    render() {
+        return (
+          <div className="SmurfList-Container">
+            <ul className="smurfList">
+              {this.props.smurfList.map((smurf, key) => {
+                return (
+                  <li className='smurf' key={key}>
+                    <h1 className="smurfname">name:{smurf.name}</h1>
+                    <h2 className="smurfinfo">age:{smurf.age}</h2>
+                    <h2 className="smurfinfo">height:{smurf.height}</h2>
+                    <h2 className="smurfinfo">id:{smurf.id}</h2>
+                  </li>
+                );
+              })}
+            </ul>
+          </div>
+        );
+      }
+    }
+
+    const mapStateToProps = state => ({
+        smurfList: state.smurfList
+    })
+
+    const mapDispatchToProps = {
+        getSmurfs
+    }
+
+    export default connect(mapStateToProps, mapDispatchToProps)(SmurfList)
